bd97cd2b0e127546843e54b448eaa2ce2d6f1706
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-07-17 23:07:17 +0500

    last

--- public/login/dashbord/dashboard.js
+++ public/login/dashbord/dashboard.js
@@ -1,4 +1,5 @@
 getdatatable()
+// getyear()
 setname()
 function setname(){ 
         const getname = localStorage.getItem('userName');
@@ -8,6 +9,7 @@ function setname(){
     }
     function logout(){
         window.location.replace('../login.html')
+        localStorage.removeItem("userId")
     }
     
     
@@ -53,22 +55,33 @@ function saveExpense(){
     const category = document.getElementById('category-expense').value
     const date = document.getElementById('date-expense').valueAsDate
     const Discription = document.getElementById('Discription-expense').value
-    
+    const getimg = document.getElementById('file-expense').files[0]
 
-    firebase.firestore().collection('transection').add({
-        amount,
-        date,
-        Discription,
-        category,
-        userid,
-        type : 'Expense'
-    }).then(function(){
-        alert("successful")
-        getdatatable();
-        clearmodel()
-        $('#expenseModal').modal('hide')
+
+    const stroageRef = firebase.storage().ref('2.jpg');
+
+    stroageRef.put(getimg).then(function(response){
+        response.ref.getDownloadURL().then(function(url){
+            console.log('url' , url)
+            firebase.firestore().collection('transection').add({
+                amount,
+                date,
+                Discription,
+                category,
+                userid,
+                type : 'Expense',
+                receipt : url
+            }).then(function(){
+                alert("successful")
+                getdatatable();
+                clearmodel()
+                $('#expenseModal').modal('hide')
+            }).catch(function(error){
+                alert('error')
+            })
+        })
     }).catch(function(error){
-        alert('error')
+        console.log('error' , error.message)
     })
 }
 function clearmodel(){
@@ -89,22 +102,29 @@ function getdatatable(){
     .then(function(snaps){
         snaps.forEach(function(doc) {
             const data = doc.data()
+            // n = data.date
+            
+            // const n = getyear.getMonth()
+            // console.log('year',n)
             const row = document.createElement('TR')
             const amount = document.createElement('TD')
             const type = document.createElement('TD')
             const category = document.createElement('TD')
             const date = document.createElement('TD')
+            const image = document.createElement('TD')
         
             amount.innerHTML = data.amount
             type.innerHTML = data.type
             category.innerHTML = data.category
             date.innerHTML = moment(data.date.toDate()).format('MMMM Do YYYY')
-            
-            
+            image.innerHTML = `<img src="${data.receipt}" width="100"/>`
+
+
             row.appendChild(amount)
             row.appendChild(type)
             row.appendChild(category)
             row.appendChild(date)
+            row.appendChild(image)
             table.appendChild(row)
         });
     })
@@ -112,8 +132,10 @@ function getdatatable(){
 function typefilter(){
     const userid = localStorage.getItem('userId')
     const type = document.getElementById('filter').value
+    // const month = document.getElementById('year').value
+    // console.log('month--->',month)
     if(type === 'all' ){
-        return getdatatable()
+        return getdatatable()   
     }
     
     const table = document.getElementById('tbody')
@@ -127,23 +149,46 @@ function typefilter(){
     .then(function(snaps){
         snaps.forEach(function(doc) {
             const data = doc.data()
+            yearget = data.date
             const row = document.createElement('TR')
             const amount = document.createElement('TD')
             const type = document.createElement('TD')
             const category = document.createElement('TD')
             const date = document.createElement('TD')            
+            const image = document.createElement('TD')
         
+
             amount.innerHTML = data.amount
             type.innerHTML = data.type
             category.innerHTML = data.category
             date.innerHTML = moment(data.date.toDate()).format('MMMM Do YYYY')
+            image.innerHTML = `<img src="${data.receipt}" width="100"/>`
+
             
             row.appendChild(amount)
             row.appendChild(type)
             row.appendChild(category)
             row.appendChild(date)
+            row.appendChild(image)
             table.appendChild(row)
         });
     });
 }
 
+// function getyear(){
+//     const userid = localStorage.getItem('userId')
+//     firebase.firestore().collection('transection')
+//     .where('userid' , '==' , userid)
+//     .get()    
+//     .then(function(snapshots){
+//         snapshots.forEach(function(doc) {
+//             const data = doc.data()
+//             const dd = data.date
+
+//             console.log(getMilliseconds(dd))
+
+            
+            
+//        })
+//     })
+// }
